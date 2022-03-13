77758d56f2d6f188276f9d8cd53cb5886aec5fdf
Aleksanderprofic <aleprofic@gmail.com>
2022-03-13 14:28:39 +0100

    Create project

---
+++ src/main/java/pl/edu/uj/ii/repositoriesanalyticssystem/RepositoriesAnalyticsSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.uj.ii.repositoriesanalyticssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoriesAnalyticsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoriesAnalyticsSystemApplication.class, args);
+    }
+
+}
